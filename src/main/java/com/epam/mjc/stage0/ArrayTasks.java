35b0f08bcba56964e387ab2b3df1eb7e4fc83f84
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-07-13 16:29:35 +0300

    Update ArrayTasks.java

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -117,6 +117,67 @@ public class ArrayTasks {
      * arr = [[5, 4], [7]]       -> [[7], [4, 5]]
      */
     public int[][] sortRaggedArray(int[][] arr) {
-
+            int[][] arr = new int[][] {{1,5,8,-6},{0,2}};
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = mergeSort(arr[i]);
+        }
+        sortLength(arr);
+        return arr;
     }
+         public static void sortLength (int [][] arr) {
+         for (int left = 0; left < arr.length; left++) {
+             // Вытаскиваем значение элемента
+             int value = arr[left].length;
+             int[] valueArr = arr[left];
+             // Перемещаемся по элементам, которые перед вытащенным элементом
+             int i = left - 1;
+             for (; i >= 0; i--) {
+                 // Если вытащили значение меньшее — передвигаем больший элемент дальше
+                 if (value < arr[i].length) {
+                     arr[i + 1] = arr[i];
+                 } else {
+                     // Если вытащенный элемент больше — останавливаемся
+                     break;
+                 }
+             }
+             // В освободившееся место вставляем вытащенное значение
+             arr[i + 1] = valueArr;
+         }
+             public static int[] mergeSort(int[] array1) {
+        int[] buffer1 = Arrays.copyOf(array1, array1.length);
+        int[] buffer2 = new int[array1.length];
+        int[] result = mergeSortInner(buffer1, buffer2, 0, array1.length);
+        return result;
+    }
+
+    public static int[] mergeSortInner(int[] buffer1, int[] buffer2,
+                                       int startIndex, int endIndex) {
+        if (startIndex >= endIndex - 1) {
+            return buffer1;
+        }
+
+        // уже отсортирован.
+        int middle = startIndex + (endIndex - startIndex) / 2;
+        int[] sorted1 = mergeSortInner(buffer1, buffer2, startIndex, middle);
+        int[] sorted2 = mergeSortInner(buffer1, buffer2, middle, endIndex);
+
+        // Слияние
+        int index1 = startIndex;
+        int index2 = middle;
+        int destIndex = startIndex;
+        int[] result = sorted1 == buffer1 ? buffer2 : buffer1;
+        while (index1 < middle && index2 < endIndex) {
+            result[destIndex++] = sorted1[index1] < sorted2[index2]
+                    ? sorted1[index1++] : sorted2[index2++];
+        }
+        while (index1 < middle) {
+            result[destIndex++] = sorted1[index1++];
+        }
+        while (index2 < endIndex) {
+            result[destIndex++] = sorted2[index2++];
+        }
+        return result;
+    }
+
+}
 }
